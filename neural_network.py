b2221ba6c274974f5db3cbc4c03ac9399eabb79f
Nick Tikhonov <ndtikhonov@gmail.com>
2020-11-13 21:40:13 +0000

    Compiles but doesn't seem to work

--- neural_network.py
+++ neural_network.py
@@ -14,7 +14,7 @@ class NeuralNetwork:
     def init_weights(self):
         weights = []
         for i in range(len(self.s) - 1):
-            weights.append(np.zeros([self.s[i + 1], self.s[i] + 1]))
+            weights.append(np.ones([self.s[i + 1], self.s[i] + 1]))
         return weights
 
     # Run a forward pass of values x
@@ -33,11 +33,10 @@ class NeuralNetwork:
     def predict(self, x):
         return self.activation(self.forward_pass(x)[-1])
 
-    def back_propagation(self, xs, ys):
+    def back_propagation(self, xs, ys, lr=0.001):
         assert len(xs) == len(ys)
         delta = self.init_weights()
         for i in range(len(xs)):
-            print("example", i)
             zs = self.forward_pass(xs[i])
             # initialise error tensor
             # to all zeros, and same shape as
@@ -45,15 +44,16 @@ class NeuralNetwork:
             error = [np.zeros(x.shape) for x in zs]
             error[-1] = self.activation(zs[-1]) - ys[i]
             for l in reversed(range(len(zs) - 1)[1:]):
-                lhs = np.matmul(self.weights[l].T, error[l + 1])[1:]
+                lhs = np.matmul(np.atleast_2d(self.weights[l]).T, error[l + 1])[1:]
                 rhs = self.activation_derivative(zs[l])
                 error[l] = lhs * rhs
 
             # There is some sort of bug here...
-            print(delta)
-            for l in range(len(delta)):
-                print(error[l + 1], zs[l].T)
-                delta[l] += np.matmul(error[l + 1], zs[l].T)
+            for l in range(len(zs) - 1):
+                delta[l] += np.matmul(np.atleast_2d(error[l + 1]).T, np.atleast_2d(np.insert(zs[l], 0, 1)))
+
+        for w in range(len(self.weights)):
+           self.weights[w] -= delta[w] * lr * 1/len(xs)
 
     def activation(self, x):
         return 1 / (1 + np.exp(-x))
@@ -62,5 +62,15 @@ class NeuralNetwork:
         return self.activation(x) * (1 - self.activation(x))
 
 if __name__ == "__main__":
-    n = NeuralNetwork([1,3,1])
-    n.back_propagation([[1],[2],[3],[4]],[[0],[0],[1],[1]])
+    n = NeuralNetwork([2,5,5,1])
+    xs = [[0,0], [0,1], [1,0], [1,1]]
+    ys = [[0], [1], [1], [0]]
+
+    for i in range(10000):
+        print(n.predict(xs[0]) + n.predict(xs[3]) - n.predict(xs[1]) - n.predict(xs[2]))
+        n.back_propagation(xs, ys)
+
+    print(n.predict(xs[0]))
+    print(n.predict(xs[1]))
+    print(n.predict(xs[2]))
+    print(n.predict(xs[3]))

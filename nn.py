b03831c860187eadea31b549b918dd088fab5993
Nick Tikhonov <ndtikhonov@gmail.com>
2020-11-28 16:41:33 +0000

    Add dropout

--- nn.py
+++ nn.py
@@ -13,6 +13,13 @@ class SigmoidActivation:
 
 
 class NN:
+    """
+    Todo:
+    - add ability to change activation functions for hidden layers
+    - add ability to provide a custom cost function
+    - add support for regularisation
+    - add support for mini-batching
+    """
     # n is the number of neurons in each layer
     def __init__(self, n):
         self.n = n
@@ -55,8 +62,15 @@ class NN:
         diff = np.sum(y_hat - ys.T)
         return (diff * diff) / len(xs)
 
-    def backprop(self, xs, ys, alpha=0.01):
+    def backprop(self, xs, ys, alpha=0.01, dropout=0):
+        weights = [np.copy(w) for w in self.weights]
+        bias = [np.copy(b) for b in self.bias]
+
         (z, a) = self.forward(xs)
+
+        if dropout > 0:
+            keep = 1 - dropout
+            a = [np.multiply(np.random.randn(ac.shape[0], ac.shape[1]) < keep, ac) / keep for ac in a]
 
         # backward pass
         m = xs.shape[0]
@@ -65,15 +79,20 @@ class NN:
         db = [None for i in range(len(z))]
 
         dz[-1] = a[-1] - ys.T
-        for i in reversed(range(len(self.weights))):
+        for i in reversed(range(len(weights))):
             ii = i + 1
             if dz[ii] is None:
-                dz[ii] = np.dot(self.weights[ii].T, dz[ii + 1]) * SigmoidActivation.d(z[ii])
+                dz[ii] = np.dot(weights[ii].T, dz[ii + 1]) * SigmoidActivation.d(z[ii])
             dw[ii] = (1/m) * np.dot(dz[ii], a[ii - 1].T)
             db[ii] = (1/m) * np.sum(dz[ii], axis=1, keepdims=True)
 
-        for i in range(len(self.weights)):
-            self.weights[i] -= (alpha * dw[i + 1])
+
+        for i in range(len(weights)):
+            weights[i] -= (alpha * dw[i + 1])
+
+        self.weights = weights
+        self.bias = bias
+            
 
 
 if __name__ == "__main__":
@@ -82,10 +101,10 @@ if __name__ == "__main__":
     age = titanic["Age"]
     xs = np.asarray([age, sex]).T
     ys = np.asarray([[x] for x in titanic["Survived"]])
-    nn = NN([2,30,30,30,1])
+    nn = NN([2,30,30,30,30,1])
 
-    for i in range(1000):
-        nn.backprop(xs, ys)
+    for i in range(10000):
+        nn.backprop(xs, ys, dropout=0.1)
         print(nn.loss(xs, ys))
 
 
